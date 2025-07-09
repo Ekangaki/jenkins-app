8d5265974208e5f65c6c4833fad2d10033326018
E Njang <georgegedeon2012@gmail.com>
2025-07-09 10:32:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins With Afro IT Solutions (AIT)
         </a>
       </header>
       <p>
